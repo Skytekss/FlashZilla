211007e127e7f1ffe9e8caf71c76691ac19091ad
Ilia Grinko <grink2000@mail.ru>
2024-11-29 14:38:08 +0300

    Add timer and minor accesebility and UI fix

--- CardView.swift
+++ CardView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CardView: View {
+  @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+  
   @State private var isShowingAnswer = false
   @State private var offset = CGSize.zero
   
@@ -17,44 +19,68 @@ struct CardView: View {
   
   var body: some View {
     ZStack {
-      RoundedRectangle(cornerRadius: Sizes.cardRectangleCornerRadius)
-        .fill(.white)
-        .shadow(radius: Sizes.cardRectangleShadowRadius)
+      cardBody
       
-      VStack {
-        Text(card.prompt)
-          .font(.largeTitle)
-          .foregroundStyle(.black)
-        
-        if isShowingAnswer {
-          Text(card.answer)
-            .font(.title)
-            .foregroundStyle(.secondary)
-        }
-      }
-      .padding()
-      .multilineTextAlignment(.center)
+      cardContent
     }
     .frame(width: Sizes.cardFrameWidth, height: Sizes.cardFrameHeight)
     .rotationEffect(.degrees(offset.width / Sizes.cardRotationDegrees))
     .offset(x: offset.width * Sizes.cardOffsetMultiple)
     .opacity(Sizes.cardOpacityMain - Double(abs(offset.width / Sizes.cardOpacityDegrees)))
     .gesture(
-      DragGesture()
-        .onChanged { gesture in
-          offset = gesture.translation
-        }
-        .onEnded { _ in
-          if abs(offset.width) > Sizes.cardDragMaxPoint {
-            removal?()
-          } else {
-            offset = .zero
-          }
-        }
+      cardGesture
     )
     .onTapGesture {
       isShowingAnswer.toggle()
     }
+  }
+  
+  // MARK: VIEWS
+  
+  var cardBody: some View {
+    RoundedRectangle(cornerRadius: Sizes.cardRectangleCornerRadius)
+      .fill(
+        accessibilityDifferentiateWithoutColor
+        ? .white
+        : .white.opacity(Sizes.cardOpacityFillMain - Double(abs(offset.width / Sizes.cardOpacityDegrees)))
+      )
+      .background(
+        accessibilityDifferentiateWithoutColor
+        ? nil
+        : RoundedRectangle(cornerRadius: Sizes.cardRectangleCornerRadius)
+          .fill(offset.width > 0 ? .green : .red)
+      )
+      .shadow(radius: Sizes.cardRectangleShadowRadius)
+  }
+  
+  var cardContent: some View {
+    VStack {
+      Text(card.prompt)
+        .font(.largeTitle)
+        .foregroundStyle(.black)
+      
+      if isShowingAnswer {
+        Text(card.answer)
+          .font(.title)
+          .foregroundStyle(.secondary)
+      }
+    }
+    .padding()
+    .multilineTextAlignment(.center)
+  }
+  
+  var cardGesture: some Gesture {
+    DragGesture()
+      .onChanged { gesture in
+        offset = gesture.translation
+      }
+      .onEnded { _ in
+        if abs(offset.width) > Sizes.cardDragMaxPoint {
+          removal?()
+        } else {
+          offset = .zero
+        }
+      }
   }
   
   private struct Sizes {
@@ -65,6 +91,7 @@ struct CardView: View {
     static let cardRotationDegrees: CGFloat = 5
     static let cardOffsetMultiple: CGFloat = 5
     static let cardOpacityMain: CGFloat = 2
+    static let cardOpacityFillMain: CGFloat = 1
     static let cardOpacityDegrees: CGFloat = 50
     static let cardDragMaxPoint: CGFloat = 100
   }
--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -8,7 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+  @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+  @Environment(\.scenePhase) var scenePhase
+  
   @State private var cards = Array<Card>(repeating: .example, count: 10)
+  @State private var timeRmaining = 100
+  @State private var isActive = true
+  
+  let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
   
   var body: some View {
     ZStack {
@@ -16,6 +23,8 @@ struct ContentView: View {
         .ignoresSafeArea()
       
       VStack {
+        timerView
+        
         ZStack {
           ForEach(0..<cards.count, id: \.self) { index in
             CardView(card: cards[index]) {
@@ -27,8 +36,64 @@ struct ContentView: View {
           }
         }
       }
+      
+      if accessibilityDifferentiateWithoutColor {
+        withoutColorAlternativeHelper
+      }
+    }
+    .onReceive(timer) { time in
+      guard isActive else { return }
+      
+      if timeRmaining > 0 {
+        timeRmaining -= 1
+      }
+    }
+    .onChange(of: scenePhase) {
+      if scenePhase == .active {
+        isActive = true
+      } else {
+        isActive = false
+      }
     }
   }
+  
+  // MARK: VIEWS
+  
+  var timerView: some View {
+    Text("Time: \(timeRmaining)")
+      .font(.title)
+      .foregroundStyle(.white)
+      .padding(.horizontal, 16)
+      .padding(.vertical, 8)
+      .background(.black.opacity(0.7))
+      .clipShape(.capsule)
+  }
+  
+  var withoutColorAlternativeHelper: some View {
+    VStack {
+      Spacer()
+      
+      HStack {
+        Image(systemName: "xmark.circle")
+          .padding()
+          .background(.black)
+          .clipShape(.circle)
+        
+        Spacer()
+        
+        Image(systemName: "checkmark.circle")
+          .padding()
+          .background(.black)
+          .clipShape(.circle)
+      }
+      .foregroundStyle(.white)
+      .font(.largeTitle)
+      .padding()
+    }
+  }
+  
+  
+  // MARK: FUNCTIONS
   
   func removeCard(at index: Int) {
     cards.remove(at: index)
